78fe21b49d46a10c2cb8ae2d5d99bf47530247e7
Akira Egawa <lucian.ming.chen@gmail.com>
2026-01-10 18:11:17 -0800

    Close buttons stuff

--- PetMenu.gd
+++ PetMenu.gd
@@ -8,6 +8,7 @@ func _ready():
 	$TalkButton.pressed.connect(func(): talk_pressed.emit())
 	$InteractButton.pressed.connect(func(): interact_pressed.emit())
 	$ScheduleButton.pressed.connect(func(): schedule_pressed.emit())
+	$"Close Button".pressed.connect(func(): schedule_pressed.emit())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
